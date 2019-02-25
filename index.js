eb58df50cd1a8fb7ed24a518cf707b486b854fd3
Cheng Vue <csalvue@gmail.com>
2019-02-25 14:17:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,9 +34,9 @@ class Route {
 
   estimatedTime(peakHours) {
     if (peakHours) {
-      return (this.blocksTravelled/2);
+      return (this.blocksTravelled()/2);
     } else {
-      return (this.blocksTravelled/3);
+      return (this.blocksTravelled()/3);
     }
   }
 }
